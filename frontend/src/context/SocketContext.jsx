cdce7bf992b29f9e1d727a70c4180e3cb4e1db1c
definitelynotchirag <chiragdave1888@gmail.com>
2024-03-15 10:48:33 +0530

    Update socket URL in SocketContext.jsx

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
     useEffect(() => {
         if(authUser){
-            const socket = io("https://localhost:3000", {
+            const socket = io("https://rc-app2.onrender.com/", {
                 query: {
                     userId: authUser._id,
                 },
