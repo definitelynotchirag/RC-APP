117446cbafae8d4e5d8daa2e2488f008afc391f8
definitelynotchirag <chiragdave1888@gmail.com>
2024-03-15 09:53:03 +0530

    Update CORS origin in socket.js

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "localhost:3000",
+    origin: "https://localhost:3000",
     methods: ["GET", "POST"],
   },
 });
