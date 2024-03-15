e89f149ed9a8befd192ea0869b75ee703b2b730c
definitelynotchirag <chiragdave1888@gmail.com>
2024-03-15 09:40:41 +0530

    Fix cookie secure flag and update build script

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -12,7 +12,7 @@ const generateTokenAndSetCookie = (userId,res) => {
         maxAge: 15*24*60*60*1000,
         httpOnly: true, //cookie cannot be accessed or modified by the browser
         sameSite : "strict", //cookie will only be sent in a first-party context
-        secure: process.env.NODE_ENV !== "development"
+        secure: false
     })
 }
 
