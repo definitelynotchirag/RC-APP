6f55b34b6e04d76341a3dd3049411a464e3f403c
definitelynotchirag <chiragdave1888@gmail.com>
2024-09-07 02:40:41 +0530

    Minor changes

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,41 +1,28 @@
-import React from "react";
 import { useAuthContext } from "../../context/AuthContext";
-import useConversation from "../../zustand/useConversation";
 import { extractTime } from "../../utils/extractTime";
+import useConversation from "../../zustand/useConversation";
 
-const Message = ({message}) => {
-  const {authUser} = useAuthContext();
-  const {selectedConversation} = useConversation();
-  const fromMe = message.from === authUser._id;
-  const formattedTime = extractTime(message.createdAt);
-  const chatClassName = fromMe ? "chat chat-start" : "chat chat-end";
-  const profilePic = fromMe ? authUser.profilePic : selectedConversation.profilePic;
+const Message = ({ message }) => {
+	const { authUser } = useAuthContext();
+	const { selectedConversation } = useConversation();
+	const fromMe = message.senderId === authUser._id;
+	const formattedTime = extractTime(message.createdAt);
+	const chatClassName = fromMe ? "chat-end" : "chat-start";
+	const profilePic = fromMe ? authUser.profilePic : selectedConversation?.profilePic;
+	const bubbleBgColor = fromMe ? "bg-blue-500" : "";
 
-  const bubblebgColor = fromMe ? "bg-purple-500" : "";
-  const shakeClass = message.shouldShake? "shake" : "";
+	const shakeClass = message.shouldShake ? "shake" : "";
 
-
-  return (
-    <div className={`${chatClassName}`}>
-      <div className="chat-image avatar">
-        <div className="w-10 rounded-full">
-          <img
-            alt="Tailwind CSS chat bubble component"
-            src={profilePic}
-          ></img>
-        </div>
-      </div>
-
-      <div className="chat-header">
-        Obi-Wan Kenobi
-       
-      </div>
-
-      <div className={`chat-bubble text-white ${bubblebgColor} ${shakeClass} pb-2`}>{message.message}</div>
-      {/* <div className="chat-footer opacity-50">Delivered</div> */}
-      <time className="chat-footer opacity-50">{formattedTime}</time>
-    </div>
-  );
+	return (
+		<div className={`chat ${chatClassName}`}>
+			<div className='chat-image avatar'>
+				<div className='w-10 rounded-full'>
+					<img alt='Tailwind CSS chat bubble component' src={profilePic} />
+				</div>
+			</div>
+			<div className={`chat-bubble text-white ${bubbleBgColor} ${shakeClass} pb-2`}>{message.message}</div>
+			<div className='chat-footer opacity-50 text-xs flex gap-1 items-center'>{formattedTime}</div>
+		</div>
+	);
 };
-
 export default Message;
--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -6,37 +6,39 @@ const SocketContext = createContext();
 
 export const useSocketContext = () => {
   return useContext(SocketContext);
-
-}
+};
 export const SocketContextProvider = ({ children }) => {
   const [socket, setSocket] = useState(null);
   const [onlineUsers, setOnlineUsers] = useState([]);
   const { authUser } = useAuthContext();
 
-    useEffect(() => {
-        if(authUser){
-            const socket = io("https://rc-app2.onrender.com/", {
-                query: {
-                    userId: authUser._id,
-                },
-                rejectUnauthorized: false,
-                transports: [ 'polling', 'flashsocket']
-            });
-
-
-            setSocket(socket);
-
-            socket.on("getOnlineUsers", (users) => {
-                setOnlineUsers(users);
-            });
-
-            return () => socket.close();
-        } else{
-            if(socket){
-                socket.close();
-                setSocket(null);
-            }
+  useEffect(() => {
+    if (authUser) {
+      const socket = io("http://localhost:5000", {
+        query: {
+          userId: authUser._id,
         }
-    },[authUser]);
-  return <SocketContext.Provider value={{socket,onlineUsers}}>{children}</SocketContext.Provider>;
+        // rejectUnauthorized: false,
+        // transports: [ 'polling', 'flashsocket']
+      });
+
+      setSocket(socket);
+
+      socket.on("getOnlineUsers", (users) => {
+        setOnlineUsers(users);
+      });
+
+      return () => socket.close();
+    } else {
+      if (socket) {
+        socket.close();
+        setSocket(null);
+      }
+    }
+  }, [authUser]);
+  return (
+    <SocketContext.Provider value={{ socket, onlineUsers }}>
+      {children}
+    </SocketContext.Provider>
+  );
 };
