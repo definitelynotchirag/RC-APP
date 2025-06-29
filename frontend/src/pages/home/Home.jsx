839a46a4f999f1da8982b114d126d478130e4a88
chirag <chiragdave1888@gmail.com>
2025-06-29 23:45:49 +0530

    feat: restructure Home component layout

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -4,9 +4,11 @@ import MessageContainer from '../../components/messages/MessageContainer'
 
 const Home = () => {
   return (
-    <div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
-        <Sidebar />
-        <MessageContainer/>
+    <div className="min-h-screen w-full flex items-center justify-center">
+      <div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+          <Sidebar />
+          <MessageContainer/>
+      </div>
     </div>
   )
 }
