4ecf35af1f9622acd30800eda23503b932cfb6cf
chirag <chiragdave1888@gmail.com>
2025-06-29 23:47:06 +0530

    style: format Home component code for consistency

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,16 +1,15 @@
-import React from 'react'
-import Sidebar from '../../components/sidebar/Sidebar'
-import MessageContainer from '../../components/messages/MessageContainer'
+import MessageContainer from "../../components/messages/MessageContainer";
+import Sidebar from "../../components/sidebar/Sidebar";
 
 const Home = () => {
-  return (
-    <div className="min-h-screen w-full flex items-center justify-center">
-      <div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
-          <Sidebar />
-          <MessageContainer/>
-      </div>
-    </div>
-  )
-}
+    return (
+        <div className="min-h-screen w-full flex items-center justify-center">
+            <div className="flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+                <Sidebar />
+                <MessageContainer />
+            </div>
+        </div>
+    );
+};
 
-export default Home
+export default Home;
